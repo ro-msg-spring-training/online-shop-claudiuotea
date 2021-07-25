1b74915b12f4b9c033fcf07483a8afbeca328a09
Otea Claudiu <claudiu_otea@yahoo.com>
2021-07-25 15:26:14 +0300

    Implemented order controller and strategy

--- shop/src/main/java/ro/msg/learning/shop/Repositories/Interfaces/IOrderDetailRepo.java
+++ shop/src/main/java/ro/msg/learning/shop/Repositories/Interfaces/IOrderDetailRepo.java
@@ -2,7 +2,6 @@ package ro.msg.learning.shop.Repositories.Interfaces;
 
 import org.springframework.data.repository.CrudRepository;
 import ro.msg.learning.shop.Models.OrderDetail;
-import ro.msg.learning.shop.Models.OrderDetailId;
 
-public interface IOrderDetail extends CrudRepository<OrderDetail, OrderDetailId> {
+public interface IOrderDetail extends CrudRepository<OrderDetail, Integer> {
 }
